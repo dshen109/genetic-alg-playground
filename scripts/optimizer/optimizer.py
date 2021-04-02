c480e8e18f7a5564e3dd7bd78b98f755afd0eab3
Daniel Shen <daniel.shen@berkeley.edu>
2021-04-01 19:52:14 -0700

    finish basic GA

--- scripts/optimizer/optimizer.py
+++ scripts/optimizer/optimizer.py
@@ -3,22 +3,26 @@ import random
 
 import numpy
 
-from deap import creator, base, tools, algorithms
+from deap import creator, base, tools
 
-# TODO: change random seed
+
+# TODO: do we need to set random seed?
+# Set seed for consistency of running things
 numpy.random.seed(0)
 random.seed(0)
 
-toolbox = base.Toolbox()
+# First weight is for error (minimize error), second weight is for validity
+creator.create('FitnessDual', base.Fitness, weights=(-1.0, 1.0))
 
 
 class RCOptimizer:
     """
     Optimize a RC circuit
     """
-    def __init__(self, B_matrix, C_matrix, D_matrix,
-                 training_data, validation_data, test_data, bounds,
-                 names, n_individuals=100, generations=50, identifier=0):
+    def __init__(self, C_matrix, D_matrix,
+                 training_data, validation_data, test_data,
+                 names, bounds=None, n_individuals=100, generations=50,
+                 identifier=0):
         """Optimize a matrix of RC values.
 
         Assumes the governing equations are of the form `xdot = A x + B u`
@@ -37,13 +41,12 @@ class RCOptimizer:
         :param float timestep: Simulation timestep
         :param int identifier: ID for this optimization
         """
-        self.B_matrix = B_matrix
         self.C_matrix = C_matrix
         self.D_matrix = D_matrix
         self.training_data = training_data
         self.validation_data = validation_data
         self.test_data = test_data
-        self.bounds = bounds
+        self.bounds = bounds or {}
         self.names = names
         self.n_individuals = n_individuals
         self.generations = generations
@@ -51,21 +54,26 @@ class RCOptimizer:
 
         self.validate_bounds()
 
+        self.rc_vals = {
+            'foo': 1, 'bar': 2
+        }
+        self.toolbox = base.Toolbox()
+        self.register()
+
     def register(self):
         """Register functions with the DEAP toolbox."""
-        toolbox.register(
-            f"individual_{self.identifier}", tools.initRepeat,
-            creator.Individual, A=self.A, B=self.B_matrix, C=self.C_matrix,
-            D=self.D_matrix, data=self.training_data, n=self.n_individuals)
-        toolbox.register('population', tools.initRepeat, list, toolbox)
+        self.toolbox.register(
+            self.individual_name, Scenario, rc_vals=self.rc_vals,
+            C=self.C_matrix, D=self.D_matrix, data=self.training_data,
+            rc_bounds=self.bounds)
+        self.toolbox.register(
+            'population', tools.initRepeat, list,
+            getattr(self.toolbox, self.individual_name)
+        )
 
     def learn(self):
         """Execute learning."""
         pass
-
-    @property
-    def A(self):
-        return numpy.random.rand((3, 3))
 
     @property
     def individual_name(self):
@@ -78,6 +86,12 @@ class RCOptimizer:
                     "First element of bound should be smaller than the second"
                     "element."
                 )
+
+    @classmethod
+    def from_yaml(self, filepath):
+        """Instantiate from a yaml"""
+        # TODO: Implement
+        raise NotImplementedError
 
 
 class Scenario:
@@ -94,31 +108,44 @@ class Scenario:
         self.D = D
         self.data = data
         self.rc_bounds = rc_bounds
+        # Maximizing fitness
+        self._fitness = creator.FitnessDual()
 
     def predict(self):
         """Simulate the output variable based on the state matrices."""
         # For now, we just return the results as the sum of the A matrix.
         # TODO: Actually simulate the output y array
-        return numpy.ones((self.steps, 1)) * sum(self.rc_vals.values())
+        return numpy.ones((1, 1)) * sum(self.rc_vals.values())
 
     def error(self):
         """Return the error between the simulated data and y array."""
         # TODO: Make this actually output the error.
-        return sum(self.predict())
+        return abs(numpy.sum(self.predict()) - 20)
 
+    def score(self):
+        """Return the fitness of the individual.
+
+        Not named as *fitness* because we need to reserve that name for DEAP.
+
+        TODO: Return tuple of score and validity.
+        """
+        if self.rc_valid:
+            validity = 1
+        else:
+            validity = 0
+        return self.error(), validity
+
+    @property
     def fitness(self):
-        """Return the fitness of the individual"""
-        if not self.rc_valid:
-            return 0
-        # TODO: Make this more efficient..? Division is slow.
-        return 1 / self.error()
+        self._fitness.values = self.score()
+        return self._fitness
 
     @property
     def rc_valid(self):
         """Return True if all RC values are within specified bounds."""
         if not self.rc_bounds:
             return True
-        for k, v in self.rc_bounds.values():
+        for k, v in self.rc_bounds.items():
             low = v[0]
             high = v[1]
             val = self.rc_vals[k]
@@ -164,9 +191,9 @@ def deap_mut_wrapper(func):
     @wraps(func)
     def wrapped(individual, *args, **kwargs):
         keys = sorted(individual.rc_vals.keys())
-        seq = func([individual.rc_vals[k] for k in keys], *args, **kwargs)
+        seq, = func([individual.rc_vals[k] for k in keys], *args, **kwargs)
         individual.rc_vals = {k: v for k, v in zip(keys, seq)}
-        return individual
+        return individual,
     return wrapped
 
 
@@ -191,8 +218,3 @@ mutGaussian = deap_mut_wrapper(tools.mutGaussian)
 mutPolynomialBounded = deap_mut_wrapper(tools.mutPolynomialBounded)
 mutShuffleIndexes = deap_mut_wrapper(tools.mutShuffleIndexes)
 mutUniformInt = deap_mut_wrapper(tools.mutUniformInt)
-
-
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", Scenario, fitness=creator.FitnessMax)
-toolbox.register("mate", cxTwoPoint)
